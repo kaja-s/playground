6d0292267db4c538c5352eb064fbbb727abf571a
Kaja Skerlj <kaja.skerlj@gmail.com>
2025-01-04 13:31:49 +0100

    Enter animation

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,27 @@
+import * as motion from "motion/react-client";
+
 export default function Home() {
-  return <h1>Hello, Kaja's playground!</h1>;
+  const ball = {
+    width: 100,
+    height: 100,
+    backgroundColor: "#dd00ee",
+    borderRadius: "50%",
+  };
+
+  return (
+    <div className="flex items-center justify-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+      <div className="flex gap-4 items-center flex-col sm:flex-row">
+        <h1>Hello World</h1>
+        <motion.div
+          initial={{ opacity: 0, scale: 0 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{
+            duration: 0.4,
+            scale: { type: "spring", visualDuration: 0.4, bounce: 0.5 },
+          }}
+          style={ball}
+        />
+      </div>
+    </div>
+  );
 }
