de2b2e6e389d5b57f0d7e80421de808804b0e29a
Kaja Skerlj <kaja.skerlj@gmail.com>
2025-01-04 11:17:01 +0100

    set up

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,8 +9,34 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        background: "var(--color-background)",
+
+        black: "var(--color-black)",
+        white: "var(--color-white)",
+
+        blue100: "var(--color-blue-100)",
+        blue200: "var(--color-blue-200)",
+        blue300: "var(--color-blue-300)",
+        blue400: "var(--color-blue-400)",
+        blue500: "var(--color-blue-500)",
+        blue700: "var(--color-blue-700)",
+
+        grey100: "var(--color-grey-100)",
+        grey200: "var(--color-grey-200)",
+        grey300: "var(--color-grey-300)",
+        grey400: "var(--color-grey-400)",
+        grey500: "var(--color-grey-500)",
+        grey600: "var(--color-grey-600)",
+        grey700: "var(--color-grey-700)",
+        grey800: "var(--color-grey-800)",
+        grey900: "var(--color-grey-900)",
+
+        ctablue: "var(--color-ctablue)",
+      },
+      fontFamily: {
+        heading: ["Open Sans", "serif"],
+        paragraph: ["Open Sans", "serif"],
+        button: ["Open Sans", "serif"],
       },
     },
   },
